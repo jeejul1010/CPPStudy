3b218cbd334385e8f2fcbe6d06e7c795e1e00be7
지승 류 <julie0402@gmail.com>
2021-10-16 13:21:49 +0900

    testing from visual studio code

---
+++ test/inheritanceTest.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+class First{
+    public:
+    void first()
+    {
+        cout<<"First()"<<endl;
+    }
+
+    virtual void myFunc(){
+        cout<<"firstFunc()"<<endl;
+    }
+};
+
+class Second: public First{
+    public:
+    void second()
+    {
+        cout<<"Second()"<<endl;
+    }
+
+    virtual void myFunc(){
+        cout<<"secondFunc()"<<endl;
+    }
+};
+
+class Third: public Second{
+    public:
+    void third()
+    {
+        cout<<"Third()"<<endl;
+    }
+
+    virtual void myFunc(){
+        cout<<"thirdFunc()"<<endl;
+    }
+};
+
+int main()
+{
+    Third obj;
+    Second & sref= obj;
+    Second* sptr = &obj;
+    sptr->myFunc();
+    //sptr->third();
+    // sref.third();
+    sref.myFunc();
+    return 0;
+}
